98b47f02808c1cef515be7a09aad2c8992d3d6c3
Rishi <rishi.helloworld@gmail.com>
2026-03-11 22:56:38 +0530

    fixed port binding

--- main.py
+++ main.py
@@ -86,4 +86,4 @@ async def ping():
     return "pong"
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
